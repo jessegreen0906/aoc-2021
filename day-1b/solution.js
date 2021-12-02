803ea9c4b49a722a5592d3ecd07d7e04210621ce
Jesse Green <jessegreen0906@gmail.com>
2021-12-03 09:18:30 +1100

    A better solution to problem 1b

--- day-1b/solution.js
+++ day-1b/solution.js
@@ -4,22 +4,19 @@ const path = require('path')
 // Load data
 const fileContents = fs.readFileSync(path.join(__dirname,'input.txt'),"utf-8") // Synchronous call, just coz. Async call would be more performant with a larger data set.
 const parsedData = fileContents.split("\r\n").map(value => {return parseInt(value)});
-const summedDepths = parsedData.map((val, i, arr) => {return arr.length > i+2 ? parsedData[i]+parsedData[i+1]+parsedData[i+2] : null})
-summedDepths.pop()
-summedDepths.pop()
+
+/* Previously I'd added each sequence of numbers together and then done iterative comparisons between them.
+    But I was a fool! If I compare the difference between n1+n2+n3 and n2+n3+n4 I'm actually just comparing n1 and n4.
+    So let's just take a comparison of value n(x) and n(x+3)
+ */
+const differences = parsedData.map((val, i, arr) => {return arr.length > i+3 ? arr[i] - arr[i+3] : 0})
 
 // Initialise
 let counter = 0
-let prevNum = summedDepths.shift()
-let currNum
 
-// Iterate through inputs
-while (summedDepths.length > 0) {
-    // Compare and count
-    currNum = summedDepths.shift()
-    prevNum < currNum ? counter++ : null
-    prevNum = currNum
-}
+differences.forEach((n) => {n<0?counter++:null})
+
+// Count how many calculated differences are negative (i.e. the second number is larger than the first)
 
 // Return
 console.log(counter)
